63cd654e2062e4cfdde63b53a4fbcbfa89886d4e
cherrymonth <1115064450@qq.com>
2018-10-24 20:10:13 +0800

    添加去哪网数据分析

---
+++ quna/randomforest_test.py
@@ -0,0 +1,97 @@
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+from sqlalchemy import create_engine
+import numpy as np
+import matplotlib.pyplot as plt
+
+engine = create_engine('sqlite:///MyDB_all_2.sqlite3', echo=True)
+
+data = pd.read_sql("select * from item;", con=engine)
+data = data[(data['num'] != 0) & (data['price'] != 0) | data['level'].strip != '']
+
+data['num'][:] = pd.Series(pd.qcut(np.array(data['num']), 5).codes)
+data["area"] = data["area"].apply(lambda x: x.replace("[", "").replace("]", ""))
+data["province"] = data["area"].apply(lambda x: x.split("·")[0])
+# data['level'] = data['level'].apply(lambda x: x.split("A")[0] if x else 0)
+data['level'] = data['level'].apply(lambda x: x if x.split("A")[0] else '3A以下景区')
+# 取特征值
+x = data[['price']]
+print(x)
+# 取目标值
+y = data[['level']]
+
+# x['age'].fillna(x['age'].mean(), axis=0, inplace=True)
+"""
+train_test_split 为划分数据集，返回四个结果
+第一个为训练特征矩阵，第二个为训练标签矩阵
+第三个为测试特征矩阵，第四个为测试标签矩阵
+
+test_size为两个数据集的比例
+random_state 为随机数种子，不同的种子得到的结果是不同的
+如果种子相同且不为零，那么结果是相同的，如果为零那么每次结果都是随机的
+"""
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+"""
+参数orient可以是字符串{'dict', 'list', 'series', 'split', 'records', 'index'}中的
+任意一种来决定字典中值的类型
+字典dict（默认）：类似于{列：{索引：值}}这样格式的字典
+列表list：类似于{列：[值]}这种形式的字典
+序列series：类似于{列：序列（值）}这种形式的字典
+分解split：类似于{索引：[索引]，列：[列]，数据：[值]}这种形式的字典
+记录records：类似于[{列：值}，...，{列：值}]这种形式的列表
+索引index：类似于{索引：{列：值}}这种形式的字典
+"""
+x_train = x_train.to_dict(orient='records')  # 加了orient="records"  以行操作
+x_test = x_test.to_dict(orient='records')
+
+from sklearn.feature_extraction import DictVectorizer
+
+decv = DictVectorizer()
+
+"""
+DictVectorizer作用是把字典类型的列表向量化
+
+[{'sex': 'female', 'age': 28.0, 'pclass': '2nd'}, 
+{'sex': 'female', 'age': 28.0, 'pclass': '2nd'}]
+
+这种格式变成
+
+[[47.          1.          0.          0.          1.          0.        ]
+ [64.          1.          0.          0.          1.          0.        ]
+ [31.19418104  0.          0.          1.          1.          0.        ]
+ [31.19418104  0.          0.          1.          0.          1.        ]]
+ 
+ 我们可以获取每一列的特征介绍
+ ['age', 'pclass=1st', 'pclass=2nd', 'pclass=3rd', 'sex=female', 'sex=male']
+ 
+ 可以发现其中非数值化数据变为单独的一个特征,但是这样导致整个矩阵变成稀疏矩阵，所以使用压缩矩阵存储
+  (0, 0)	0.8333
+  (0, 2)	1.0
+  (0, 5)	1.0
+ 
+ 可以发现，这是一个三元组（数据结构的压缩矩阵表示方法）
+ 其中每一行中第一个元素是非0值的坐标，第二个为值
+"""
+
+x_train = decv.fit_transform(x_train)  # 先拟合，后标准化
+x_test = decv.transform(x_test)
+
+rf = RandomForestClassifier(n_estimators=10)  # max_depth=10最大树深,
+rf_model = rf.fit(x_train, y_train)
+y_pred = rf.predict(x_test)
+score = rf.score(x_test, y_test)
+print(score)
+# 打印报告
+# from sklearn.metrics import classification_report
+
+"""
+精确度: 表示的是预测为正的样本中有多少是真正的正样本。
+召回率:表示的是样本中的正例有多少被预测正确了。
+F1值:2*精度*召回率/(精度+召回率)
+支持数: 该类在样本中出现的总次数
+"""
+# print(len(np.where(y_test == 0)[0]))
+# print(y_test)
+# print(classification_report(y_true=y_test, y_pred=y_pred))
