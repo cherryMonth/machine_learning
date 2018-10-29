b839800b5bffc56ea738da41d3849b4234a5728b
cherrymonth <1115064450@qq.com>
2018-10-29 15:33:31 +0800

    update

--- quna/randomforest_test.py
+++ quna/randomforest_test.py
@@ -1,11 +1,21 @@
-from sklearn.model_selection import train_test_split
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+from sklearn.feature_extraction import DictVectorizer
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 import pandas as pd
-from sqlalchemy import create_engine
-import numpy as np
+# 网址可以直接复制
+data = pd.read_csv('http://biostat.mc.vanderbilt.edu/wiki/pub/Main/DataSets/titanic.txt')
+# 取特征值
+x = data[['pclass','age','sex']]
+# 取目标值
+y = data[['survived']]
+x['age'].fillna(x['age'].mean(),axis=0,inplace=True)
 
-
-# x['age'].fillna(x['age'].mean(), axis=0, inplace=True)
 """
 train_test_split 为划分数据集，返回四个结果
 第一个为训练特征矩阵，第二个为训练标签矩阵
@@ -15,7 +25,9 @@ test_size为两个数据集的比例
 random_state 为随机数种子，不同的种子得到的结果是不同的
 如果种子相同且不为零，那么结果是相同的，如果为零那么每次结果都是随机的
 """
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2)
+
 
 """
 参数orient可以是字符串{'dict', 'list', 'series', 'split', 'records', 'index'}中的
@@ -27,12 +39,9 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 记录records：类似于[{列：值}，...，{列：值}]这种形式的列表
 索引index：类似于{索引：{列：值}}这种形式的字典
 """
-x_train = x_train.to_dict(orient='records')  # 加了orient="records"  以行操作
+
+x_train = x_train.to_dict(orient='records')     # 加了orient="records"  以行操作
 x_test = x_test.to_dict(orient='records')
-
-from sklearn.feature_extraction import DictVectorizer
-
-decv = DictVectorizer()
 
 """
 DictVectorizer作用是把字典类型的列表向量化
@@ -59,24 +68,49 @@ DictVectorizer作用是把字典类型的列表向量化
  其中每一行中第一个元素是非0值的坐标，第二个为值
 """
 
+decv = DictVectorizer()
+
 x_train = decv.fit_transform(x_train)  # 先拟合，后标准化
 x_test = decv.transform(x_test)
 
 rf = RandomForestClassifier(n_estimators=10)  # max_depth=10最大树深,
-rf_model = rf.fit(x_train, y_train)
+rf.fit(x_train, y_train)
 y_pred = rf.predict(x_test)
 score = rf.score(x_test, y_test)
 print(score)
-# 打印报告
-from sklearn.metrics import classification_report
 
+# 打印报告
 """
 精确度: 表示的是预测为正的样本中有多少是真正的正样本。
 召回率:表示的是样本中的正例有多少被预测正确了。
 F1值:2*精度*召回率/(精度+召回率)
 支持数: 该类在样本中出现的总次数
 """
+from sklearn.metrics import classification_report
+print(classification_report(y_true=y_test,y_pred=y_pred))
 
-# print(len(np.where(y_test == 0)[0]))
-# print(y_test)
-print(classification_report(y_true=y_test, y_pred=y_pred))
+from sklearn.externals import joblib  # 保存模型，下次直接就能使用无需训练
+
+joblib.dump(rf, 'random_forest.pkl')
+
+
+# In[5]:
+
+
+from sklearn.externals import joblib
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.0001)
+x_train = x_train.to_dict(orient='records')     # 加了orient="records"  以行操作
+x_test = x_test.to_dict(orient='records')
+print(len(x))
+x_train = decv.fit_transform(x_train)  # 先拟合，后标准化
+x_test = decv.transform(x_test)
+rf_load = joblib.load('random_forest.pkl')
+score = rf_load.score(x_train, y_train)
+print(score)
+
+
+# In[ ]:
+
+
+
+
