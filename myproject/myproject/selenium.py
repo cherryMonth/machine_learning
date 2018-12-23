d4d4dc5c67d7505ec5faa8ee97c3639266c55777
cherrymonth <1115064450@qq.com>
2018-12-23 13:45:35 +0800

    修复bug

--- myproject/myproject/selenium.py
+++ myproject/myproject/selenium.py
@@ -9,6 +9,7 @@ class Selenium(object):
         option_chrome = webdriver.ChromeOptions()
         option_chrome.add_argument('--headless')
         option_chrome.add_argument('--no-startup-window')
+        option_chrome.add_argument('--no-sandbox')
         driver = webdriver.Chrome(chrome_options=option_chrome)
         driver.get(request.url)
         content = driver.page_source.encode('utf-8')
